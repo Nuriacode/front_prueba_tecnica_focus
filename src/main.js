4d766c7f6415e742be29a53bc63f27a89ca731de
Nuria Castaño <codesalnuria@gmail.com>
2023-06-22 17:44:03 +0200

    test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VueAxios from "vue-axios";
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios);
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
