f10547025e3e88cd0a29617f765dbfeaf11fa3b0
Nuria Castaño <codesalnuria@gmail.com>
2023-06-22 18:27:39 +0200

    add new view LogIn

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from "./App.vue";
 // import router from "./router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import VueAxios from "vue-axios";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.use(VueAxios);
+Vue.use(VueAxios, axios);
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
